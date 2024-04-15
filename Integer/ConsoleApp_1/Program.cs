d3e32556edec030286a324e86fe5e0c5643e3fff
Aida <barieva97al@gmail.com>
2024-04-15 18:02:59 +0600

    learning for Integer

---
+++ Integer/ConsoleApp_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано расстояние L в сантиметрах. Используя операцию делениянацело, найти
+// количество полных метров в нем (1 метр = 100 см)
+
+using System;
+
+namespace ConsoleApp_1
+{
+    class Programm
+    {
+        static void Main(string[] argh)
+        {
+             //  int L, Lm; почему нельзя это написать?
+              Console.Write("L: ");
+              int L = int.Parse(Console.ReadLine());
+              int Lm = L / 100;
+              Console.WriteLine("Lm:" + Lm);
+              Console.ReadKey();
+        }
+    }
+}
