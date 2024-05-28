bd96c31b451b0b7d3fa5d4886ed05526cf156794
Aida <barieva97al@gmail.com>
2024-05-28 14:43:35 +0500

    work on "Boolean"

---
+++ Boolean/Boolean_App/ConsoleApp_5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два целых числа: A, B. Проверить истинность высказывания: «Справедливы неравенства A ≥ 0 или B < −2».
+
+using System;
+
+namespace ConsoleApp_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите число А: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число В: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            bool c = a >= 0 || b < -2;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
